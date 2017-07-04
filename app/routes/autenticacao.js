c87af630041a2ae40de97de503614e798bd2223a
claudiodantas <claudio_dantas@hotmail.com>
2017-07-04 14:16:42 -0300

    can you poupe me?

---
+++ app/routes/autenticacao.js
@@ -0,0 +1,10 @@
+/*can you poupe me?
+povo pidão
+eu tein é ódio
+
+commit -> git add --ignore-removal .
+git commit -m "can you poupe me?"
+
+enviar para o servidor do git -> git push origin master
+
+comando pra pegar alterações -> git pull*/
