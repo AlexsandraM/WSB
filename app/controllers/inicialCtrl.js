1823413c9091515b41e2716fbfe0f2eded3a6be3
claudiodantas <claudio_dantas@hotmail.com>
2017-07-26 11:48:27 -0300

    Delete inicialCtrl.js

--- app/controllers/inicialCtrl.js
+++
@@ -1,9 +0,0 @@
-angular.module('wsb').controller('loginCtrl', loginCtrl);
-
-function loginCtrl($scope, $rootScope, $routeParams, $location, authSvc) {
-  var self = this;
-
-  self.show = function(){
-          $location.path('/welcome');
-  }
-}
